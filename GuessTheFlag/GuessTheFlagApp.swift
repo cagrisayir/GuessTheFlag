4af29aa94b92d36e8bd363445a6f928f2addade9
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-02 11:33:25 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Omer Cagri Sayir on 2.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
